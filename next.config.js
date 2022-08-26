8f1c63cc6bd56e2ac0366e7de9e62f2befa285cb
joacode <jperezprimavera@gmail.com>
2022-08-25 21:55:02 -0300

    Config and some dependencies
    Config and create/findAll services

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 }
 
-const withPWA = require("next-pwa");
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withPWA = require('next-pwa')
 
 module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-});
+    pwa: {
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+    },
+    publicRuntimeConfig: {
+        apiBaseUrl: process.env.MONGO_URI,
+    },
+})
 
 module.exports = nextConfig
