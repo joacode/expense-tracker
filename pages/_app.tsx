8e3fba2fb893168356ffd9aaadbddc7ba468ea48
joacode <jperezprimavera@gmail.com>
2022-08-29 15:56:17 -0300

    Refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import Head from 'next/head'
 import React, { ReactElement } from 'react'
 import '../styles/globals.css'
 
 export default function MyApp(props): ReactElement {
     const { Component, pageProps } = props
-
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Financial Tool</title>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png" />
+                <meta name="theme-color" content="#fff" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
